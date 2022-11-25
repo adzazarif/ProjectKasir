d5490671ecb7fb358d4a6d7cd49ab8def772449a
adzazarif <adzazarf@gmail.com>
2022-11-25 22:28:46 +0700

    update

---
+++ src/koneksi/conn.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class conn {
+       private static Connection mysqlconfig;
+    public static Connection configDB() throws SQLException{
+        try {
+            String url = "jdbc:mysql://localhost/db_login";
+            String user = "root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig = DriverManager.getConnection(url,user,pass);
+        } catch (Exception e) {
+            System.err.println("koneksi gagal " + e.getLocalizedMessage());
+        }
+        return mysqlconfig;
+}
+}
